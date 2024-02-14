dc687d9d55a212c16dabb8e23c2cb02b3e816037
orhanbalci <orhanbalci@gmail.com>
2024-02-14 22:27:48 +0300

    refactor from genawaiter to generator crate, move escape code handling functions to parser_listener

--- src/parser.rs
+++ src/parser.rs
@@ -1,48 +1,54 @@
 #![allow(clippy::cmp_owned)]
 
-use genawaiter::yield_;
+use std::sync::{Arc, Mutex};
+
+use generator::{Generator, Gn};
 
 use crate::control::*;
 use crate::parser_listener::ParserListener;
 
-pub struct Parser<T: ParserListener> {
-    listener: T,
+pub struct ParserState {
     use_utf8: bool,
 }
+pub struct Parser<'a> {
+    parser_fsm: Generator<'a, String, Option<bool>>,
+    parser_state: Arc<Mutex<ParserState>>,
+}
 
-impl<T: ParserListener> Parser<T> {
-    pub fn new(listener: T, use_utf8: bool) -> Self {
-        Parser { listener, use_utf8 }
-    }
-
-    pub fn start(&self) {
-        let mut printer = genawaiter::sync::Gen::new(genawaiter::sync_producer!({
+impl<'a> Parser<'a> {
+    pub fn start<T>(&mut self, listener: Arc<Mutex<T>>)
+    where
+        T: ParserListener + Send + 'a,
+    {
+        self.parser_state = Arc::new(Mutex::new(ParserState { use_utf8: true }));
+        let parser_state_cloned = self.parser_state.clone();
+        self.parser_fsm = Gn::<String>::new_scoped(move |mut co| {
             loop {
-                let mut char: &str = yield_!(Some(true));
+                let mut char = co.yield_(Some(true)).unwrap_or_default();
                 if ESC == char {
-                    char = yield_!(None);
+                    char = co.yield_(None).unwrap_or_default();
                     if char == "[" {
-                        char = CSI;
+                        char = CSI.to_owned();
                     } else if char == "]" {
-                        char = OSC;
+                        char = OSC.to_owned();
                     } else {
                         if char == "#" {
-                            if yield_!(None) == DECALN {
-                                self.listener.alignment_display();
+                            if co.yield_(None).unwrap_or_default() == DECALN {
+                                listener.lock().unwrap().alignment_display();
                             } else {
                                 println!("unexpected escape character");
                             }
                         } else if char == "%" {
-                            self.select_other_charset(yield_!(None));
-                        } else if "()".contains(char) {
-                            let code = yield_!(None);
-                            if self.use_utf8 {
+                            // self.select_other_charset(yield_!(None));
+                        } else if "()".contains(&char) {
+                            let code = co.yield_(None);
+                            if parser_state_cloned.lock().unwrap().use_utf8 {
                                 continue;
                             } else {
-                                self.listener.define_charset(code, char);
+                                // listener.lock().unwrap().define_charset(code, char);
                             }
                         } else {
-                            self.escape_dispatch(char);
+                            listener.lock().unwrap().escape_dispatch(&char);
                         }
                         continue;
                     }
@@ -51,26 +57,26 @@ impl<T: ParserListener> Parser<T> {
                     if char == SI || char == SO {
                         continue;
                     } else {
-                        self.basic_dispatch(char);
+                        listener.lock().unwrap().basic_dispatch(&char);
                     }
                 } else if char == CSI {
                     let mut params: Vec<u32> = vec![];
                     let mut private: bool = false;
                     let mut current: String = "".to_owned();
                     loop {
-                        char = yield_!(None);
+                        char = co.yield_(None).unwrap_or_default();
                         if char == "?" {
                             private = true;
                         } else if ALLOWED_IN_CSI.iter().any(|cf| *cf == char) {
-                            self.basic_dispatch(char);
+                            listener.lock().unwrap().basic_dispatch(&char);
                         } else if char == SP || char == GREATER {
                         } else if char == CAN || char == SUB {
-                            self.listener.draw(char);
+                            listener.lock().unwrap().draw(&char);
                             break;
-                        } else if char.chars().nth(0).unwrap().is_digit(10) {
-                            current.push(char.chars().nth(0).unwrap());
-                        } else if (char == "$") {
-                            yield_!(None);
+                        } else if char.chars().next().unwrap().is_ascii_digit() {
+                            current.push(char.chars().next().unwrap());
+                        } else if char == "$" {
+                            co.yield_(None);
                             break;
                         } else {
                             let mut current_param = match current.parse::<u32>() {
@@ -83,184 +89,42 @@ impl<T: ParserListener> Parser<T> {
                                 current = "".to_owned();
                             } else {
                                 if private {
-                                    self.csi_dispatch(char, &params[..], true);
+                                    listener
+                                        .lock()
+                                        .unwrap()
+                                        .csi_dispatch(&char, &params[..], true);
                                 } else {
-                                    self.csi_dispatch(char, &params[..], false);
+                                    listener.lock().unwrap().csi_dispatch(
+                                        &char,
+                                        &params[..],
+                                        false,
+                                    );
                                 }
                                 break;
                             }
                         }
                     }
                 } else if char == OSC {
-                    let code = yield_!(None);
-                    if code == "R" {
+                    let code = co.yield_(None).unwrap_or_default();
+                    if code == "R" || code == "p" {
                         continue; // reset palette not implemented
-                    } else if code == "p" {
-                        continue; // set palette not implemented
                     }
-
-                    let mut param = "".to_owned();
+                    let _param = "".to_owned();
 
                     loop {
-                        let mut accu = String::from(yield_!(None));
+                        let mut accu = co.yield_(None).unwrap_or_default();
                         if accu == ESC {
-                            accu.push_str(yield_!(None));
+                            accu.push_str(&co.yield_(None).unwrap_or_default());
                         }
                     }
                 }
             }
-        }));
-
-        printer.resume_with("h");
-        printer.resume_with("w");
+        });
     }
 
-    fn select_other_charset(&self, input: &str) {}
-
-    fn escape_dispatch(&self, escape_command: &str) {
-        match escape_command {
-            ec if ec == RIS => {
-                self.listener.reset();
-            }
-            ec if ec == IND => {
-                self.listener.index();
-            }
-            ec if ec == NEL => {
-                self.listener.linefeed();
-            }
-            ec if ec == RI => {
-                self.listener.reverse_index();
-            }
-            ec if ec == HTS => {
-                self.listener.set_tab_stop();
-            }
-            ec if ec == DECSC => {
-                self.listener.save_cursor();
-            }
-            ec if ec == DECRC => {
-                self.listener.restore_cursor();
-            }
-            _ => {
-                println!("un expected escape code")
-            }
-        }
-    }
-
-    fn basic_dispatch(&self, basic_command: &str) {
-        match basic_command {
-            ec if ec == BEL => {
-                self.listener.bell();
-            }
-            ec if ec == BS => {
-                self.listener.backspace();
-            }
-            ec if ec == HT => {
-                self.listener.tab();
-            }
-            ec if (ec == LF || ec == VT || ec == FF) => {
-                self.listener.linefeed();
-            }
-            ec if ec == CR => {
-                self.listener.cariage_return();
-            }
-            _ => {
-                println!("un expected escape code")
-            }
-        }
-    }
-
-    fn csi_dispatch(&self, csi_command: &str, params: &[u32], is_private: bool) {
-        match csi_command {
-            ec if ec == ICH => self.listener.insert_characters(if !params.is_empty() {
-                Some(params[0])
-            } else {
-                None
-            }),
-            ec if ec == CUD => self.listener.cursor_up(if !params.is_empty() {
-                Some(params[0])
-            } else {
-                None
-            }),
-            ec if ec == CUU => self.listener.cursor_down(if !params.is_empty() {
-                Some(params[0])
-            } else {
-                None
-            }),
-            ec if ec == CUF => self.listener.cursor_forward(if !params.is_empty() {
-                Some(params[0])
-            } else {
-                None
-            }),
-            ec if ec == CUB => self.listener.cursor_back(if !params.is_empty() {
-                Some(params[0])
-            } else {
-                None
-            }),
-            ec if ec == CNL => self.listener.cursor_down1(if !params.is_empty() {
-                Some(params[0])
-            } else {
-                None
-            }),
-            ec if ec == CPL => self.listener.cursor_up1(if !params.is_empty() {
-                Some(params[0])
-            } else {
-                None
-            }),
-            ec if ec == CHA => self.listener.cursor_to_column(if !params.is_empty() {
-                Some(params[0])
-            } else {
-                None
-            }),
-            ec if ec == CUP => {
-                if !params.is_empty() {
-                    self.listener
-                        .cursor_position(Some(params[0]), Some(params[1]));
-                } else {
-                    self.listener.cursor_position(None, None)
-                }
-            }
-            ec if ec == ED => self.listener.erase_in_display(if !params.is_empty() {
-                Some(params[0])
-            } else {
-                None
-            }),
-            ec if ec == EL => self.listener.erase_in_line(if !params.is_empty() {
-                Some(params[0])
-            } else {
-                None
-            }),
-            ec if ec == IL => self.listener.insert_lines(if !params.is_empty() {
-                Some(params[0])
-            } else {
-                None
-            }),
-            ec if ec == DL => self.listener.delete_lines(if !params.is_empty() {
-                Some(params[0])
-            } else {
-                None
-            }),
-            ec if ec == DCH => self
-                .listener
-                .delete_characters(params.iter().cloned().next()),
-            ec if ec == ECH => self
-                .listener
-                .erase_characters(params.iter().cloned().next()),
-            ec if ec == HPR => self.listener.cursor_forward(params.iter().cloned().next()),
-            ec if ec == DA => self
-                .listener
-                .report_device_attributes(params.iter().cloned().next()),
-            ec if ec == VPA => self.listener.cursor_to_line(params.iter().cloned().next()),
-            ec if ec == VPR => self.listener.cursor_down(params.iter().cloned().next()),
-            ec if ec == HVP => self
-                .listener
-                .cursor_position(params.iter().cloned().nth(0), params.iter().cloned().nth(1)),
-            ec if ec == TBC => self.listener.clear_tab_stop(params.iter().cloned().next()),
-            ec if ec == SM => self.listener.set_mode(params),
-            ec if ec == RM => self.listener.reset_mode(params),
-            ec if ec == SGR => self.listener.select_graphic_rendition(params),
-            _ => {
-                println!("unexpected csi escape code");
-            }
-        }
+    pub fn feed(&mut self, input: String) {
+        self.parser_fsm.send(input);
     }
 }
+
+// fn select_other_charset(&self, input: &str) {}
--- src/parser_listener.rs
+++ src/parser_listener.rs
@@ -1,3 +1,44 @@
+use crate::control::{
+    BEL,
+    BS,
+    CHA,
+    CNL,
+    CPL,
+    CR,
+    CUB,
+    CUD,
+    CUF,
+    CUP,
+    CUU,
+    DA,
+    DCH,
+    DECRC,
+    DECSC,
+    DL,
+    ECH,
+    ED,
+    EL,
+    FF,
+    HPR,
+    HT,
+    HTS,
+    HVP,
+    ICH,
+    IL,
+    IND,
+    LF,
+    NEL,
+    RI,
+    RIS,
+    RM,
+    SGR,
+    SM,
+    TBC,
+    VPA,
+    VPR,
+    VT,
+};
+
 pub trait ParserListener {
     fn alignment_display(&self);
     fn define_charset(&self, code: &str, mode: &str);
@@ -39,4 +80,144 @@ pub trait ParserListener {
     fn set_mode(&self, modes: &[u32]);
     fn reset_mode(&self, modes: &[u32]);
     fn select_graphic_rendition(&self, modes: &[u32]);
+
+    fn escape_dispatch(&self, escape_command: &str) {
+        match escape_command {
+            ec if ec == RIS => {
+                self.reset();
+            }
+            ec if ec == IND => {
+                self.index();
+            }
+            ec if ec == NEL => {
+                self.linefeed();
+            }
+            ec if ec == RI => {
+                self.reverse_index();
+            }
+            ec if ec == HTS => {
+                self.set_tab_stop();
+            }
+            ec if ec == DECSC => {
+                self.save_cursor();
+            }
+            ec if ec == DECRC => {
+                self.restore_cursor();
+            }
+            _ => {
+                println!("un expected escape code")
+            }
+        }
+    }
+
+    fn basic_dispatch(&self, basic_command: &str) {
+        match basic_command {
+            ec if ec == BEL => {
+                self.bell();
+            }
+            ec if ec == BS => {
+                self.backspace();
+            }
+            ec if ec == HT => {
+                self.tab();
+            }
+            ec if (ec == LF || ec == VT || ec == FF) => {
+                self.linefeed();
+            }
+            ec if ec == CR => {
+                self.cariage_return();
+            }
+            _ => {
+                println!("un expected escape code")
+            }
+        }
+    }
+
+    fn csi_dispatch(&self, csi_command: &str, params: &[u32], is_private: bool) {
+        match csi_command {
+            ec if ec == ICH => self.insert_characters(if !params.is_empty() {
+                Some(params[0])
+            } else {
+                None
+            }),
+            ec if ec == CUD => self.cursor_up(if !params.is_empty() {
+                Some(params[0])
+            } else {
+                None
+            }),
+            ec if ec == CUU => self.cursor_down(if !params.is_empty() {
+                Some(params[0])
+            } else {
+                None
+            }),
+            ec if ec == CUF => self.cursor_forward(if !params.is_empty() {
+                Some(params[0])
+            } else {
+                None
+            }),
+            ec if ec == CUB => self.cursor_back(if !params.is_empty() {
+                Some(params[0])
+            } else {
+                None
+            }),
+            ec if ec == CNL => self.cursor_down1(if !params.is_empty() {
+                Some(params[0])
+            } else {
+                None
+            }),
+            ec if ec == CPL => self.cursor_up1(if !params.is_empty() {
+                Some(params[0])
+            } else {
+                None
+            }),
+            ec if ec == CHA => self.cursor_to_column(if !params.is_empty() {
+                Some(params[0])
+            } else {
+                None
+            }),
+            ec if ec == CUP => {
+                if !params.is_empty() {
+                    self.cursor_position(Some(params[0]), Some(params[1]));
+                } else {
+                    self.cursor_position(None, None)
+                }
+            }
+            ec if ec == ED => self.erase_in_display(if !params.is_empty() {
+                Some(params[0])
+            } else {
+                None
+            }),
+            ec if ec == EL => self.erase_in_line(if !params.is_empty() {
+                Some(params[0])
+            } else {
+                None
+            }),
+            ec if ec == IL => self.insert_lines(if !params.is_empty() {
+                Some(params[0])
+            } else {
+                None
+            }),
+            ec if ec == DL => self.delete_lines(if !params.is_empty() {
+                Some(params[0])
+            } else {
+                None
+            }),
+            ec if ec == DCH => self.delete_characters(params.iter().cloned().next()),
+            ec if ec == ECH => self.erase_characters(params.iter().cloned().next()),
+            ec if ec == HPR => self.cursor_forward(params.iter().cloned().next()),
+            ec if ec == DA => self.report_device_attributes(params.iter().cloned().next()),
+            ec if ec == VPA => self.cursor_to_line(params.iter().cloned().next()),
+            ec if ec == VPR => self.cursor_down(params.iter().cloned().next()),
+            ec if ec == HVP => {
+                self.cursor_position(params.iter().cloned().nth(0), params.iter().cloned().nth(1))
+            }
+            ec if ec == TBC => self.clear_tab_stop(params.iter().cloned().next()),
+            ec if ec == SM => self.set_mode(params),
+            ec if ec == RM => self.reset_mode(params),
+            ec if ec == SGR => self.select_graphic_rendition(params),
+            _ => {
+                println!("unexpected csi escape code");
+            }
+        }
+    }
 }
